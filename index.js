cb02b621ea02d877de7e59f3cc66591fdc1b1563
Yuhan224 <fir3panic@gmail.com>
2023-10-12 09:04:21 -0400

    10/12

---
+++ index.js
@@ -0,0 +1,53 @@
+const pokemon = [
+
+    {
+        name: "Mewtwo", 
+        mid: "no",
+        legendary: true,
+        mega: true,
+        caught: false,
+    },
+
+    {
+        name: "Pikachu",
+        mid: "yes",
+        legendary: false,
+        mega: false,
+        caught: true,
+    },
+    
+    {
+        name: "Landorus",
+        mid: "yes",
+        legendary: true,
+        mega: false,
+        caught: true,
+    },
+    
+    {
+        name: "Lucario",
+        mid: "no",
+        legendary: true,
+        mega: true,
+        caught: true,
+    },
+
+    {
+        name: "Arceus",
+        mid: "no",
+        legendary: true,
+        mega: false,
+        caught: true,
+    },
+
+    pokemon.forEach((pokemons) =>
+        console.log(legend.name)
+
+
+
+
+
+
+
+
+]
